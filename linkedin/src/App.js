a254150c9168c9edbc880056b022ea193c1e7d85
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-05-10 10:40:23 +0100

    .

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 import './css/App.css';
 import Profile from './pages/Profile';
 
 function App() {
   return (
-   <Profile/>
+    <>
+      {/* <Profile /> */}
+      <Footer />
+    </>
   );
 }
 
