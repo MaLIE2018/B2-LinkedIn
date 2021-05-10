fad2d63926ee117dd2340bb829a37c8e30cf1dfc
Hasib <hashhash506@gmail.com>
2021-05-10 11:52:59 +0100

    hasib

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/App.css';
+import "./css/App.css";
+import ProfileTop from "./components/ProfileTop";
+
 
 function App() {
   return (
-    <div>
-    </div>
-  );
+		<div>
+			<ProfileTop />
+		</div>
+	);
 }
 
 export default App;
