77f4e887c31af5a1c7dbf5c37f82b60f27e7efd6
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-05-10 13:15:57 +0200

    Added Navbar ...

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,13 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/MyNavbar';
 import './css/App.css';
 import Profile from './pages/Profile';
 import {Container} from "react-bootstrap"
 function App() {
+<<<<<<< Updated upstream
   return (
     <Container sm='fluid'>
     <Profile/>
     </Container>
   );
+=======
+	return (
+		<div>
+			<MyNavbar />
+		</div>
+	);
+>>>>>>> Stashed changes
 }
 
 export default App;
