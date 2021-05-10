f4142000fe05b303d66569303e77e2786eb77866
MaLIE2018 <liebsch@pm.me>
2021-05-10 11:34:41 +0200

    icons

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
 import Profile from './pages/Profile';
-
+import {Container} from "react-bootstrap"
 function App() {
   return (
-   <Profile/>
+    <Container sm='fluid'>
+    <Profile/>
+    </Container>
   );
 }
 
