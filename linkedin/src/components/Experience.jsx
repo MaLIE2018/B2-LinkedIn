6f8b1cdcc6c7e4df0e988ed23ba8fb8f14ea7966
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-05-11 14:03:51 +0200

    Added Experience Items...

--- linkedin/src/components/Experience.jsx
+++ linkedin/src/components/Experience.jsx
@@ -1,9 +1,58 @@
-import React, { Component } from "react";
-import Box from "../components/parts/Box";
+import React, { Component } from 'react';
+import Box from '../components/parts/Box';
 class Experience extends Component {
-  render() {
-    return <Box title='Experience' children={<div>Dadsda</div>} />;
-  }
+	state = {
+		experience: {
+			role: 'Software Engineer',
+			company: 'AXAS GmbH',
+			startDate: '01/01/2020',
+			endDtae: '01/01/2022',
+			description: 'Try now',
+			area: 'Frankfurt',
+		},
+	};
+
+	postExp = async () => {
+		try {
+			const requestProfile = await fetch(
+				'https://striveschool-api.herokuapp.com/api/profile/me',
+				{
+					method: 'GET',
+					headers: {
+						Authorization:
+							'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDlhNWViM2RmY2NjNTAwMTVhNmJiYmEiLCJpYXQiOjE2MjA3Mjk1MjQsImV4cCI6MTYyMTkzOTEyNH0.boEO9mTiItNdEDrhQcw1KIvBKIGJ0dCkRW7d3cNzv0M',
+					},
+				}
+			);
+			if (requestProfile.ok) {
+				const response = await requestProfile.json();
+				const identity = response._id;
+
+				const newUrl =
+					'https://striveschool-api.herokuapp.com/api/profile/' +
+					identity +
+					'/experiences';
+				const postExperience = await fetch(newUrl, {
+					method: 'POST',
+					headers: {
+						Authorization:
+							'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDlhNWViM2RmY2NjNTAwMTVhNmJiYmEiLCJpYXQiOjE2MjA3Mjk1MjQsImV4cCI6MTYyMTkzOTEyNH0.boEO9mTiItNdEDrhQcw1KIvBKIGJ0dCkRW7d3cNzv0M',
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(this.state.experience),
+				});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	componentDidMount() {
+		this.postExp();
+	}
+	render() {
+		return <Box title="Experience" children={<div>Dadsda</div>} />;
+	}
 }
 
 Experience.propTypes = {};
--- linkedin/src/components/ProfileTop.jsx
+++ linkedin/src/components/ProfileTop.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Component } from "react";
 import "../css/ProfileTop.css";
 import {
@@ -12,6 +13,17 @@ import {
 import { Camera, PencilOutline } from "react-ionicons";
 import Box from "../components/parts/Box";
 import LinkButton from "../components/parts/LinkButton";
+=======
+import { Component } from 'react';
+import '../css/ProfileTop.css';
+import { Row, Col, Dropdown, Button, Alert } from 'react-bootstrap';
+import CarouselBadge from './Carousel';
+import CameraIcon from './CamerIcon';
+import PencilIcon from './PencilIcon';
+import Box from '../components/parts/Box';
+import LinkButton from '../components/parts/LinkButton';
+
+>>>>>>> Stashed changes
 export default class ProfileTop extends Component {
   constructor(props) {
     super(props);
@@ -20,6 +32,7 @@ export default class ProfileTop extends Component {
     };
   }
 
+<<<<<<< Updated upstream
   getProfile = async () => {
     try {
       const requestProfile = await fetch(
@@ -96,6 +109,70 @@ export default class ProfileTop extends Component {
                       id='dropdown-basic'>
                       Open to
                     </Dropdown.Toggle>
+=======
+	getProfile = async () => {
+		try {
+			const requestProfile = await fetch(
+				'https://striveschool-api.herokuapp.com/api/profile/me',
+				{
+					method: 'GET',
+					headers: {
+						Authorization:
+							' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDk4ZmE0MTYxOWU1ZDAwMTUxZjhmN2YiLCJpYXQiOjE2MjA2MzgyNzMsImV4cCI6MTYyMTg0Nzg3M30.D-RniP4L8eJ8XOdOjRXswq8LsRnPVK-QYiUr8h9fPhk',
+					},
+				}
+			);
+			if (requestProfile.ok) {
+				const response = await requestProfile.json();
+				this.setState({ profile: response });
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	componentDidMount() {
+		this.getProfile();
+	}
+
+	render() {
+		return (
+			<Box
+				padding={false}
+				children={
+					<Row className="m-0 p-0">
+						<Col id="ProfileBackground" sx={12}>
+							<img
+								id="profileImage"
+								src={this.state.profile.image}
+								alt="profile_image"
+							/>
+							<CameraIcon classname={'cameraIcon'} />
+							<PencilIcon classname={'Pencil'} />
+						</Col>
+						<Col id="ProfileInfo" xs={12}>
+							<h3>
+								{this.state.profile.name}
+								{'  '}
+								{this.state.profile.surname}
+							</h3>
+							<h5 style={{ fontWeight: '400' }}>{this.state.profile.title}</h5>
+							<p className="d-flex align-items-center">
+								{this.state.profile.area}
+								{' - '}
+								{<LinkButton title={'500 connections'} />}
+								{' - '}
+								{<LinkButton title={'Contact info'} />}
+							</p>{' '}
+							<span className="d-flex flex-row">
+								<Dropdown>
+									<Dropdown.Toggle
+										style={{ borderRadius: '50px', marginRight: '10px' }}
+										variant="primary"
+										id="dropdown-basic"
+									>
+										Open to
+									</Dropdown.Toggle>
+>>>>>>> Stashed changes
 
                     <Dropdown.Menu>
                       <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
@@ -108,6 +185,7 @@ export default class ProfileTop extends Component {
                     </Dropdown.Menu>
                   </Dropdown>
 
+<<<<<<< Updated upstream
                   <Dropdown className='mr-1'>
                     <Dropdown.Toggle
                       style={{ borderRadius: "50px", marginRight: "10px" }}
@@ -116,6 +194,17 @@ export default class ProfileTop extends Component {
                       id='dropdown-basic'>
                       Add profile section
                     </Dropdown.Toggle>
+=======
+								<Dropdown className="mr-1">
+									<Dropdown.Toggle
+										style={{ borderRadius: '50px', marginRight: '10px' }}
+										variant="outline-dark"
+										border="dark"
+										id="dropdown-basic"
+									>
+										Add profile section
+									</Dropdown.Toggle>
+>>>>>>> Stashed changes
 
                     <Dropdown.Menu>
                       <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
@@ -128,6 +217,7 @@ export default class ProfileTop extends Component {
                     </Dropdown.Menu>
                   </Dropdown>
 
+<<<<<<< Updated upstream
                   <Button
                     style={{ borderRadius: "50px", marginRight: "10px" }}
                     variant='outline-dark'>
@@ -167,4 +257,22 @@ export default class ProfileTop extends Component {
       </>
     );
   }
+=======
+								<Button
+									style={{ borderRadius: '50px', marginRight: '10px' }}
+									variant="outline-dark"
+								>
+									More...
+								</Button>
+							</span>
+							<Row>
+								<CarouselBadge />
+							</Row>
+						</Col>
+					</Row>
+				}
+			/>
+		);
+	}
+>>>>>>> Stashed changes
 }
