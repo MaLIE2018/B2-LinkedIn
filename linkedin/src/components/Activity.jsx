a2f71b9d0696766b5a8ded25c70c8f29bddd18fd
MaLIE2018 <liebsch@pm.me>
2021-05-14 16:47:34 +0200

    activity

--- linkedin/src/components/Activity.jsx
+++ linkedin/src/components/Activity.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Box from "./parts/Box";
 import LinkButton from "./parts/LinkButton";
-const Activity = () => {
+const Activity = (props) => {
   return (
     <Box
       edit={false}
       subtitle={<LinkButton title={"296 followers"} color='#67A0D9' />}
       title={"Activity"}
       footerText={"See all Activity"}
-      render={(state) => <div>"Test"</div>}
+      render={(state) => (
+        <div className='d-flex flex-row'>
+          <img
+            src={props.profile.image}
+            alt=''
+            className='img-fluid'
+            style={{ height: "70px" }}
+          />
+          <a href='/' style={{ color: "black" }}>
+            <p className='ml-5'>Some more text</p>
+          </a>
+        </div>
+      )}
     />
   );
 };
