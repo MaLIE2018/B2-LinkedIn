7f426147adfcaa251c2cb8c42f73b428a92bac5c
MaLIE2018 <liebsch@pm.me>
2021-05-14 16:40:36 +0200

    floating parts

--- linkedin/src/components/Ad.jsx
+++ linkedin/src/components/Ad.jsx
@@ -6,12 +6,7 @@ class Ad extends Component {
   render() {
     return (
       <div className='text-center mt-2'>
-        <LinkButton
-          title='Need Developers ASAP? Hire the top 3% of 
-        developers in 48 hours. $0
-          Recruiting fee. Start now.'
-          color='black'
-        />
+        <LinkButton title={this.props.title} color='black' />
         <span className='text-muted ml-4' style={{ cursor: "pointer" }}>
           Ad
           <EllipsisHorizontalOutline
