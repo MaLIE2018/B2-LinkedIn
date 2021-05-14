1896be15df84733919d8e03b092e88484ebf849f
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-05-14 12:31:43 +0200

    Added WelcomeBox

--- linkedin/src/pages/Feed.jsx
+++ linkedin/src/pages/Feed.jsx
@@ -1,90 +1,91 @@
-import React, { Component } from "react";
-import { Col, Row } from "react-bootstrap";
-import AddToYourFeed from "../components/AddToYourFeed";
-import MyNewsFeed from "./../components/MyNewsFeed";
-import Box from "./../components/parts/Box";
-import Posts from "./../components/Posts";
+import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import AddToYourFeed from '../components/AddToYourFeed';
+import MyNewsFeed from './../components/MyNewsFeed';
+
+import Posts from './../components/Posts';
+import Welcome from './../components/WelcomeBox';
 class Feed extends Component {
-  state = {
-    posts: [],
-    updated: false,
-  };
+	state = {
+		posts: [],
+		updated: false,
+	};
 
-  getPosts = async () => {
-    const requestPosts = await fetch(
-      "https://striveschool-api.herokuapp.com/api/posts/",
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + this.props.bearerToken,
-        },
-      }
-    );
-    if (requestPosts.ok) {
-      const resp = await requestPosts.json();
-      this.setState({ posts: resp.reverse(), updated: false });
-    }
-  };
-  componentDidMount = () => {
-    this.getPosts();
-    document.title = "Linkedin - Feed";
-  };
+	getPosts = async () => {
+		const requestPosts = await fetch(
+			'https://striveschool-api.herokuapp.com/api/posts/',
+			{
+				method: 'GET',
+				headers: {
+					Authorization: 'Bearer ' + this.props.bearerToken,
+				},
+			}
+		);
+		if (requestPosts.ok) {
+			const resp = await requestPosts.json();
+			this.setState({ posts: resp.reverse(), updated: false });
+		}
+	};
+	componentDidMount = () => {
+		this.getPosts();
+		document.title = 'Linkedin - Feed';
+	};
 
-  componentDidUpdate = (prevProps, prevState) => {
-    if (prevState.updated !== this.state.updated) {
-      this.getPosts();
-    }
-  };
+	componentDidUpdate = (prevProps, prevState) => {
+		if (prevState.updated !== this.state.updated) {
+			this.getPosts();
+		}
+	};
 
-  getMyPosts = () => {
-    const posts = this.state.posts;
-    this.setState((state) => {
-      return {
-        posts: posts.filter((post) => {
-          if (post.user?._id) {
-            return post.user._id === this.props.profile._id;
-          }
-        }),
-      };
-    });
-  };
+	getMyPosts = () => {
+		const posts = this.state.posts;
+		this.setState((state) => {
+			return {
+				posts: posts.filter((post) => {
+					if (post.user?._id) {
+						return post.user._id === this.props.profile._id;
+					}
+				}),
+			};
+		});
+	};
 
-  handleUpdate = (e, bool) => {
-    e.preventDefault();
-    this.setState((state) => {
-      return { updated: bool };
-    });
-  };
+	handleUpdate = (e, bool) => {
+		e.preventDefault();
+		this.setState((state) => {
+			return { updated: bool };
+		});
+	};
 
-  render() {
-    return (
-      <Row>
-        <Col md={3}>
-          <Box render={(state) => <div>sometext</div>} />
-        </Col>
-        <Col md={6}>
-          <Posts
-            bearerToken={this.props.bearerToken}
-            profile={this.props.profile}
-            rounded={true}
-            onPostsClick={this.getMyPosts}
-            onHandleUpdate={this.handleUpdate}
-          />
-          <MyNewsFeed
-            posts={this.state.posts}
-            bearerToken={this.props.bearerToken}
-            profile={this.props.profile}
-            rounded={true}
-            onPostsClick={this.getMyPosts}
-            onHandleUpdate={this.handleUpdate}
-          />
-        </Col>
-        <Col md={3}>
-          <AddToYourFeed />
-        </Col>
-      </Row>
-    );
-  }
+	render() {
+		return (
+			<Row>
+				<Col md={3}>
+					<Welcome render={(state) => <div>sometext</div>} />
+				</Col>
+				<Col md={6}>
+					<Posts
+						bearerToken={this.props.bearerToken}
+						profile={this.props.profile}
+						rounded={true}
+						onPostsClick={this.getMyPosts}
+						onHandleUpdate={this.handleUpdate}
+					/>
+					<MyNewsFeed
+						posts={this.state.posts}
+						bearerToken={this.props.bearerToken}
+						profile={this.props.profile}
+						rounded={true}
+						onPostsClick={this.getMyPosts}
+						onHandleUpdate={this.handleUpdate}
+					/>
+				</Col>
+				<Col md={3}>
+					<AddToYourFeed />
+				</Col>
+			</Row>
+		);
+	}
 }
 
 export default Feed;
